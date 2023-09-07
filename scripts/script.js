547858d5ac683f52af20e555430f22a18acc57ca
W1sungit <127198531+W1sungit@users.noreply.github.com>
2023-09-07 06:08:31 +0000

    home test

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 const mainTitle = document.getElementById('main-title')
 
 document.addEventListener('click', () => {
-    mainTitle.innerText = 'JavaScript в действии'
+    mainTitle.innerText = 'JavaScript работает'
 })
